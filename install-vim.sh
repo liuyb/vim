e42fb1d5ce3b962eb9f709ef7d1493c231be1483
liuyb <liuyb@me.com>
2014-12-11 00:46:34 +0800

    Edit install shell

--- install-vim.sh
+++ install-vim.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-version=0.9.4
+version=0.0.1
+
+echo "Backup old ~/.vim & ~/.vimrc"
+mv ~/.vim ~/.vim.bak.liuyb
+mv ~/.vimrc ~/.vimrc.bak.liuyb
+
+echo "git clone vim from https://github.com/liuyb/vim"
+git clone https://github.com/liuyb/vim ~/.vim
+
+echo "link ~/.vim/vimrc to ~/.vim"
+ln -s ~/.vim/vimrc ~/.vimrc
+
+echo "finish! please run vim"
+
+
 
